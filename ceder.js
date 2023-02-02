18b892f6b3c3e465522f9b2a95256510b910b7e7
CederC <122538914+CederC@users.noreply.github.com>
2023-02-02 14:50:56 +0100

    Create ceder.js

---
+++ ceder.js
@@ -0,0 +1,15 @@
+var drum1 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/827[kb]050_barbituate-beat.wav.mp3")
+
+var drum2 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/1658[kb]050_glitch-hop-serenade.wav.mp3")
+
+var drum3 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/823[kb]050_udu_groove-1.wav.mp3")
+
+var drum4 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/1536[kb]055_brain-broken-skank.wav.mp3")
+
+var drum5 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/354[kb]059_808-hiphop.wav.mp3")
+
+var drum6 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/1382[kb]060_Granloopripped_B.wav.mp3")
+
+var drum7 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/692[kb]060_super-laidback.wav.mp3")
+
+var drum8 = new Audio("https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/000%20to%20080%20bpm/2680[kb]062_sonar-beat-basstalk.wav.mp3")
